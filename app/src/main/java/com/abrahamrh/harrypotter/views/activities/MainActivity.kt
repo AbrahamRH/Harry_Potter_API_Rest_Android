91db5879ac853585f9d8db418fec72ddb9b4d4ab
AbrahamRH <abrahamrzhz@gmail.com>
2023-06-03 17:57:36 -0600

    Adding the folder structure of the project

--- app/src/main/java/com/abrahamrh/harrypotter/views/activities/MainActivity.kt
+++ app/src/main/java/com/abrahamrh/harrypotter/views/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.abrahamrh.harrypotter
+package com.abrahamrh.harrypotter.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/main/java/com/abrahamrh/harrypotter/views/activities/Splash.kt
+++ app/src/main/java/com/abrahamrh/harrypotter/views/activities/Splash.kt
@@ -1,4 +1,4 @@
-package com.abrahamrh.harrypotter
+package com.abrahamrh.harrypotter.views.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
