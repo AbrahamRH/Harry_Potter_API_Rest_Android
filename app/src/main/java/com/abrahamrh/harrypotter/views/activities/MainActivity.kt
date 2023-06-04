549028259c98e1a19a0dd9403068ad2a3439ec7b
AbrahamRH <abrahamrzhz@gmail.com>
2023-06-03 20:47:23 -0600

    Adding the onClick methods to the buttons

--- app/src/main/java/com/abrahamrh/harrypotter/views/activities/MainActivity.kt
+++ app/src/main/java/com/abrahamrh/harrypotter/views/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.abrahamrh.harrypotter.views.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.abrahamrh.harrypotter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun students(view: View) {
+        val intent = Intent(this, CharactersList::class.java)
+        startActivity(intent)
+    }
+    fun profesors(view: View) {
+        val intent = Intent(this, CharactersList::class.java)
+        startActivity(intent)
     }
 }
