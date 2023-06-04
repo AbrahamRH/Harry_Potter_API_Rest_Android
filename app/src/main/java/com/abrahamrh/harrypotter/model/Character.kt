4db27987d504fed82218bda9685d07c92f860380
AbrahamRH <abrahamrzhz@gmail.com>
2023-06-03 18:35:45 -0600

    Adding the model.

---
+++ app/src/main/java/com/abrahamrh/harrypotter/model/Character.kt
@@ -0,0 +1,10 @@
+package com.abrahamrh.harrypotter.model
+
+data class Character(
+    var id: String?,
+    var image: String?,
+    var name: String?,
+    var house: String?,
+    var actor: String?,
+    var gender: String?
+)
---
+++ app/src/main/java/com/abrahamrh/harrypotter/model/CharacterDetail.kt
@@ -0,0 +1,12 @@
+package com.abrahamrh.harrypotter.model
+
+data class CharacterDetail(
+    var image: String?,
+    var name: String?,
+    var house: String?,
+    var species: String?,
+    var dateOfBirth: String?,
+    var patronus: String?,
+    var hairColour: String?,
+    var ancestry: String?,
+)
